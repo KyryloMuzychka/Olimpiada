27dce4902c1a2e4a36fb631ec941e513e2aadf10
Kyrylo Muzychka <kyryll2016@gmail.com>
2023-07-21 22:16:51 +0300

    KPI-Open/2023

---
+++ KPI-Open/2023/b.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <vector>
+#include <deque>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+#define ll long long
+#define vi vector<ll>
+#define pii pair<ll, ll>
+#define all(v) v.begin(), v.end()
+
+using namespace std;
+
+void Solve() {
+    ll N, M, S, A, B, amount = 0;    
+    deque<pair<ll, ll>> cost;
+    vi dif;
+    cin >> N >> M >> S;
+    ll t = N;
+    while (t--) {
+        cin >> A >> B;
+        cost.push_back(make_pair(A, B));        
+    }
+
+    sort(all(cost), [](const pii& pair1, const pii& pair2) {
+        return pair1.second < pair2.second;
+        });
+
+    ll dronesBought = 0;
+    for (ll i = 0; i < N; i++) {
+        if (M) {            
+            if (cost[i].second <= S) {
+                S -= cost[i].second;
+                M--;
+                dronesBought++;
+            }
+        }
+        else {
+            
+            if (cost[i].first <= S) {
+                S -= cost[i].first;
+                dronesBought++;
+            }
+        }
+        
+    }
+
+    cout << dronesBought;
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    Solve();
+    return 0;
+}
---
+++ KPI-Open/2023/d.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <vector>
+#include <deque>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <unordered_set>
+#define ll long long
+#define vi vector<ll>
+#define pii pair<ll, ll>
+#define all(v) v.begin(), v.end()
+
+using namespace std;
+
+struct Point {
+    ll x, y;
+};
+
+
+int countIntegerPoints(const std::vector<Point>& points) {
+    std::unordered_set<int> uniquePoints;
+
+    for (const auto& point : points) {
+        uniquePoints.insert(point.x * 100000 + point.y);
+    }
+
+    return uniquePoints.size();
+}
+
+void Solve() {
+    ll n;
+    cin >> n;
+
+    vector<Point> points;
+    for (ll i = 0; i < n; i++) {
+        ll x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+
+        if (x1 == x2) {
+            ll min_y = min(y1, y2);
+            ll max_y = max(y1, y2);
+            for (ll y = min_y; y <= max_y; y++) {
+                points.push_back({ x1, y });
+            }
+        }
+        else if (y1 == y2) {
+            ll min_x = min(x1, x2);
+            ll max_x = max(x1, x2);
+            for (ll x = min_x; x <= max_x; x++) {
+                points.push_back({ x, y1 });
+            }
+        }
+        else {
+            ll min_x = min(x1, x2);
+            ll max_x = max(x1, x2);
+            ll min_y = min(y1, y2);
+            ll max_y = max(y1, y2);
+
+            for (ll x = min_x; x <= max_x; x++) {
+                double y = (((double)(y2 - y1) * (x - x1)) / (x2 - x1)) + y1;
+                if (floor(y) == y) {
+                    points.push_back({ x, (ll)y });
+                }
+            }
+        }
+    }
+
+    cout << countIntegerPoints(points);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    Solve();
+    return 0;
+}
